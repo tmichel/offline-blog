66a95f9bd53468553ca62379c68aaeda026a5d70
Tamas Michelberger <tomi.michel@gmail.com>
2013-08-21 12:54:57 +0200

    added read command to start the web app

--- lib/cli/blog_cli.rb
+++ lib/cli/blog_cli.rb
@@ -2,6 +2,7 @@ require "rubygems"
 require "bundler/setup"
 require "thor"
 require "cli/post"
+require "web/app"
 
 module CLI
   class BlogCLI < Thor
@@ -9,6 +10,21 @@ module CLI
     def new(title)
       post = Post.new Dir.pwd, title
       post.create!
+    end
+
+    desc "read [POST]", "Start the webserver and open the browser."
+    long_desc <<-EOF
+    Start the webserver and open the browser.
+
+    A post can be given in the form of the filename. If given, the specfic post
+    will open in the browser.
+    EOF
+    def read(post = nil)
+      m = /\d{3,}/.match(post)
+      tail = "/entry/#{m}" if post && m
+      Sinatra::Application.run! :port => 4004 do |server|
+        system "x-www-browser http://localhost:4004#{tail} &"
+      end
     end
   end
 end
